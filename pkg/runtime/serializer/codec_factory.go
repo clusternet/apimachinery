e1bf48c40b5fbbcd735f0ec8f7db678f65e698ec
Di Xu <stephenhsu90@gmail.com>
2021-08-14 16:00:57 +0800

    remove protobuf serializer for Clusternet

--- pkg/runtime/serializer/codec_factory.go
+++ pkg/runtime/serializer/codec_factory.go
@@ -112,6 +112,16 @@ func newSerializersForScheme(scheme *runtime.Scheme, mf json.MetaFactory, option
 		},
 	}
 
+	// remove protobuf serializer for Clusternet
+	var serializersWithoutProtobuf []serializerType
+	for _, s := range serializers {
+		if s.ContentType == runtime.ContentTypeProtobuf {
+			continue
+		}
+		serializersWithoutProtobuf = append(serializersWithoutProtobuf, s)
+	}
+	serializers = serializersWithoutProtobuf
+
 	for _, fn := range serializerExtensions {
 		if serializer, ok := fn(scheme); ok {
 			serializers = append(serializers, serializer)
